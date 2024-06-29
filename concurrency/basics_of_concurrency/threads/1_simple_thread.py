a374b6683129fe75a8ab7a236e31d92f87162e33
luchanos <lukash_91@mail.ru>
2024-06-29 18:08:06 +0200

    code

--- concurrency/basics_of_concurrency/threads/1_simple_thread.py
+++ concurrency/basics_of_concurrency/threads/1_simple_thread.py
@@ -1,14 +1,14 @@
-from multiprocessing import Process
+from threading import Thread
 import os
 
 
 def some_cpu_bound_task(n: int) -> int:
-    print(f"Process with id {os.getpid()} has been started!")
+    print(f"Thread with id {os.getpid()} has been started!")
     return sum([1 for _ in range(n)])
 
 
 if __name__ == "__main__":
     n = 1_000_000_000
-    process = Process(target=some_cpu_bound_task, args=(n,))
+    thread = Thread(target=some_cpu_bound_task, args=(n,))
     print(f"This is the process {os.getpid()}")
-    process.start()
+    thread.start()
---
+++ concurrency/basics_of_concurrency/threads/2_simple_list_of_threads.py
@@ -0,0 +1,21 @@
+from threading import Thread
+import os
+import time
+
+
+def some_cpu_bound_task(n: int) -> int:
+    print(f"Thread with id {os.getpid()} has been started!")
+    return sum([1 for _ in range(n)])
+
+
+if __name__ == "__main__":
+    start = time.time()
+    n = 1_000_000_000
+
+    # Run 3 processes simultaneously
+    threads_list = []
+    for _ in range(3):
+        thread = Thread(target=some_cpu_bound_task, args=(n, ))
+        threads_list.append(thread)
+        thread.start()
+    print(f"Total execution time: {time.time() - start}")
