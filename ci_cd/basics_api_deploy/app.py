0c4be634f2ba7a2b517d24f32ceee1905aa0fc99
luchanos <lukash_91@mail.ru>
2024-09-10 00:56:32 +0200

    code

--- ci_cd/basics_api_deploy/app.py
+++ ci_cd/basics_api_deploy/app.py
@@ -9,5 +9,10 @@ async def ping():
     return {"Success": True}
 
 
+@app.get("/ping2")
+async def ping():
+    return {"Success": True}
+
+
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
